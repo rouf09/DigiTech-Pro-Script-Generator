c06e8abbf20c85d05d0d447f65693b40dccf22a3
Digitech-Pro <digitechpro19@gmail.com>
2025-11-05 03:03:30 +0600

    Update dev-run.ts

--- digitechpro-scriptgen-electron/electron/dev-run.ts
+++ digitechpro-scriptgen-electron/electron/dev-run.ts
@@ -2,6 +2,6 @@
 import { spawn } from "child_process";
 import * as path from "path";
 const electronBinary = require("electron");
-const args = [path.join(__dirname, "main.cjs")];
+const args = [path.join(__dirname, "main.js")];
 const proc = spawn(electronBinary, args, { stdio: "inherit" });
 proc.on("close", (code) => process.exit(code ?? 0));
